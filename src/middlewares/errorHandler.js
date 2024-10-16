4c7e4ef17a05a6559eba42105f253d3cb6e4e200
prihodko_bohdan <prihodkob15@gmail.com>
2024-10-16 09:09:03 +0300

    errorHandler middlewares

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,10 @@
 export const errorHandler = (err, req, res, next) => {
-  res.status(500).json({
-    status: 500,
-    message: 'Something went wrong',
+  const status = err.status || 500;
+  const message = err.message || 'Something went wrong';
+
+  res.status(status).json({
+    status,
+    message,
     data: err.message,
   });
 };
