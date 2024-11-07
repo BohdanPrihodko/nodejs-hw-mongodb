5ec6c73c8b00a9120514979aa164040e833f150b
prihodko_bohdan <prihodkob15@gmail.com>
2024-11-07 14:34:41 +0200

    rewrite services-take out ctrlWrapper

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,27 +1,26 @@
-// src/services/contacts.js
-import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import Contact from '../db/models/contacts.js';
 
 // Отримання всіх контактів користувача
-export const getAllContacts = ctrlWrapper(async (userId) => {
+export const getAllContacts = async (userId) => {
   return await Contact.find({ userId });
-});
+};
 
 // Отримання контакту за ID
-export const getContactById = ctrlWrapper(async (contactId, userId) => {
+export const getContactById = async (contactId, userId) => {
   return await Contact.findOne({ _id: contactId, userId });
-});
+};
 
 // Оновлення контакту за ID
-export const updateContactById = ctrlWrapper(async (contactId, updates, userId) => {
+export const updateContactById = async (contactId, updates, userId) => {
   return await Contact.findOneAndUpdate({ _id: contactId, userId }, updates, { new: true });
-});
+};
 
 // Видалення контакту за ID
-export const deleteContactById = ctrlWrapper(async (contactId, userId) => {
+export const deleteContactById = async (contactId, userId) => {
   return await Contact.findOneAndDelete({ _id: contactId, userId });
-});
+};
 
 // Створення нового контакту з userId
-export const createContact = (contactData, userId) =>
-  Contact.create({ ...contactData, userId });
+export const createContact = async (contactData, userId) => {
+  return await Contact.create({ ...contactData, userId });
+};
