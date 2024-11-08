c30735f2f2eba6dcee6a59022d2004acdd782a0d
prihodko_bohdan <prihodkob15@gmail.com>
2024-11-08 15:16:52 +0200

    create new branch

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 
+// схема юзера
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
